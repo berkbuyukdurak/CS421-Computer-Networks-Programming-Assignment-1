c1a51614221c82a85eda38703a367d347b9e536d
bugraaydin <bugraaydin.cs@gmail.com>
2019-04-04 04:40:11 +0300

    done the message receive part

--- src/SeekAndDestroy.java
+++ src/SeekAndDestroy.java
@@ -1,35 +1,155 @@
+import javax.xml.crypto.Data;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 
 public class SeekAndDestroy
 {
-    public static String CR = "\r";
-    public static String LF = "\n";
-    public static int controlPort = 6000;
-    public static int dataPort = 53462;
-    public static String host = "localhost";
+    final static int controlPort = 60000;
+    final static int dataPort = 53462;
+    final static String host = "localhost";
+    final static String CR = "\r";
+    final static String LF = "\n";
+
+    Queue<String> directoryQueue = new PriorityQueue<String>();
     String command;
     Socket clientSocket;
-    ServerSocket clientDataSocket;
+    ServerSocket serverSocket;
+    OutputStreamWriter outToServer;
 
-    /**
-     * In the control port client connects to server as a client sends string commands
-     * Command string generation and output processing etc. are defined below.
-     *
-     * In data connection Server connects to client ! (Reversed)
-     * */
+    // Default Constructor
+    public SeekAndDestroy(int controlPort, int dataPort, String host)
+    {
+
+        try {
+            clientSocket = new Socket(host, controlPort);
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    // TODO: IF command requests data, then should listen to that one after calling this
+    public void sendStringToPort(String str)
+    {
+        try {
+            outToServer = new OutputStreamWriter(clientSocket.getOutputStream(), "US-ASCII");
+            outToServer.write(str, 0, str.length());
+            outToServer.flush();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public ArrayList<String> readPort(){
+         try {
+             ArrayList<String> result = new ArrayList<>();
+             Socket s = serverSocket.accept();
+             InputStream is = s.getInputStream();
+             InputStreamReader isr = new InputStreamReader(is, "US-ASCII");
+             BufferedReader buffer = new BufferedReader(isr);
+             while(true) {
+                 String line = buffer.readLine();
+                 if (line == null || line.equals(""))
+                         break;
+                 result.add(line);
+             }
+             return result;
+         }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    //////////////////////////////////////////////////
+    ///////////////             //////////////////////
+    //////////////////////////////////////////////////
+    public void sendUserName(String username)
+    {
+        command = "USER" + " " + username + CR + LF;
+        sendStringToPort(command);
+    }
+    public void sendPass(String password)
+    {
+        command = "PASS" + " " + password + CR + LF;
+        sendStringToPort(command);
+    }
+    public void sendPort(int port)
+    {
+        command = "PORT" + " " + port + CR + LF;
+        sendStringToPort(command);
+    }
+    public ArrayList<String> nlst()
+    {
+        ArrayList<String> toReturn = new ArrayList<>();
+        command = "NLST" + CR + LF;
+        sendStringToPort(command);
+        ArrayList<String> output = readPort();
+        return output;
+
+    }
+    public void cwd(String child)
+    {
+        command = "CWD" + " " + child + CR + LF;
+        sendStringToPort(command);
+    }
+    public void cdup()
+    {
+        command = "CDUP" + " " + CR + LF;
+        sendStringToPort(command);
+    }
+    public void retrieve(String filename)
+    {
+        command = "RETR" + filename + CR + LF;
+        sendStringToPort(command);
+    }
+    public void delete(String filename)
+    {
+        command = "DELE" + filename + CR + LF;
+        sendStringToPort(command);
+    }
+    public void quit()
+    {
+        command = "QUIT" + CR + LF;
+        sendStringToPort(command);
+    }
+
+    public int findTarget(){
+        ArrayList<String> directories = nlst();
+        System.out.println(directories.size());
+        if(directories.size() > 0){
+            directories.set(0, directories.get(0).substring(2));
+        }
+        for(int i = 0; i < directories.size(); i++){
+
+            String[] directoryContent = directories.get(i).split(":");
+            System.out.println(directoryContent[0] +"~"+ directoryContent[1]);
+
+        }
+        return 1;
+    }
+
+    //////////////////////////////////////////////////
+    ///////////////             //////////////////////
+    /////////////////////////////////////////////////
+
     public static void main(String[] args)
     {
-        // sad is a class object to call functions from
-        SeekAndDestroy sad = new SeekAndDestroy();
+        SeekAndDestroy sad = new SeekAndDestroy(controlPort, dataPort, host);
         try
         {
             sad.sendUserName("bilkent");
             sad.sendPass("cs421");
-            sad.clientDataSocket = new ServerSocket(dataPort);
+            sad.serverSocket = new ServerSocket(dataPort);
             sad.sendPort(dataPort);
             //sad.nlst();
+            sad.findTarget();
+            //sad.cdup();
             sad.quit();
 
         }
@@ -37,93 +157,6 @@ public class SeekAndDestroy
         {
             ioe.printStackTrace();
             System.out.println("Connection Failure");
-        }
-    }
-    // Default Constructor
-    public SeekAndDestroy()
-    {
-        try {
-            clientSocket = new Socket(host, controlPort); // this is control connection
-        }catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
-    /**
-     * Commands
-     * They are self explanatory in context of the project
-     * */
-    private void sendUserName(String username)
-    {
-        command = "USER" + " " + username + CR + LF;
-        sendStringToPort(command);
-    }
-    private void sendPass(String password)
-    {
-        command = "PASS" + " " + password + CR + LF;
-        sendStringToPort(command);
-    }
-    private void sendPort(int port)
-    {
-        command = "PORT" + " " + port + CR + LF;
-        sendStringToPort(command);
-    }
-    private void nlst()
-    {
-        command = "NLST" + CR + LF;
-        sendStringToPort(command);
-    }
-    private void cwd(String child)
-    {
-        command = "CWD" + child + CR + LF;
-        sendStringToPort(command);
-    }
-    private void cdup()
-    {
-        command = "CDUP" + CR + LF;
-        sendStringToPort(command);
-    }
-    private void retrieve(String filename)
-    {
-        command = "RETR" + filename + CR + LF;
-        sendStringToPort(command);
-    }
-    private void delete(String filename)
-    {
-        command = "DELE" + filename + CR + LF;
-        sendStringToPort(command);
-    }
-    private void quit()
-    {
-        command = "QUIT" + CR + LF;
-        sendStringToPort(command);
-    }
-    // TODO: IF command requests data, then should listen to that one after calling this
-    private InputStream sendStringToPort(String str)
-    {
-        try {
-            OutputStreamWriter outputStreamWriter =
-                    new OutputStreamWriter(clientSocket.getOutputStream()
-                            , "UTF-8");
-            outputStreamWriter.write(str, 0, str.length()); // Send command
-            outputStreamWriter.flush();
-
-            InputStream inputStream = clientSocket.getInputStream();
-
-            // Following print for debug, can delete
-            // The function returns the stream returned as a result
-
-            InputStreamReader inputStreamReader =
-                    new InputStreamReader(inputStream);
-            BufferedReader bufferedReader =
-                    new BufferedReader(inputStreamReader);
-
-            System.out.println(bufferedReader.readLine());
-            return inputStream;
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-            return null;
         }
     }
 }
